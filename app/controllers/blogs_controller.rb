4da900d1f33ee45bded833da5848bf157dec9318
yusuke nakano <yuusukechrono@gmail.com>
2020-01-18 07:38:34 +0000

    共通処理

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,6 @@
 class BlogsController < ApplicationController
-  
+  before_action :set_blog, only: [:show, :edit, :update]
+
   def index
      @blogs = Blog.all
   end
@@ -18,15 +19,12 @@ class BlogsController < ApplicationController
   end
 
   def show
-    @blog = Blog.find(params[:id])
   end
 
   def edit
-    @blog = Blog.find(params[:id])
   end
 
   def update
-    @blog = Blog.find(params[:id])
     if @blog.update(blog_params)
       redirect_to blogs_path, notice: "編集しました！"
     else
@@ -38,6 +36,10 @@ class BlogsController < ApplicationController
 
   def blog_params
     params.require(:blog).permit(:content)
+  end
+
+  def set_blog
+    @blog = Blog.find(params[:id])
   end
 
 end
