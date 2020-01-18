f0528de22079339e3a50169f1b7f5b847edb5640
yusuke nakano <yuusukechrono@gmail.com>
2020-01-18 05:53:30 +0000

    一覧表示の作成

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,7 +1,7 @@
 class BlogsController < ApplicationController
   
   def index
-     
+     @blogs = Blog.all
   end
 
   def new
@@ -9,13 +9,19 @@ class BlogsController < ApplicationController
   end
 
   def create
-    Blog.create(content: params[:blog][:content])
+    Blog.create(blog_params)
     redirect_to new_blog_path
     #一覧に後で変更
   end
 
   def show
 
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:content)
   end
 
 end
