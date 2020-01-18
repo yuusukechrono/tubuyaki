a8a3328c37fa96c26222356c8bab7fbf8207b30d
yusuke nakano <yuusukechrono@gmail.com>
2020-01-18 05:47:47 +0000

    create機能の作成

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,7 +5,13 @@ class BlogsController < ApplicationController
   end
 
   def new
-  
+    @blog = Blog.new
+  end
+
+  def create
+    Blog.create(content: params[:blog][:content])
+    redirect_to new_blog_path
+    #一覧に後で変更
   end
 
   def show
